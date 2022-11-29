e6749d0651730d53cb3ce160367d4d2d20e4f5e7
Rohit Talreja <rohittalreja1313@gmail.com>
2022-11-29 10:02:09 +0530

    Day 28 Update in Code

--- Problem Solving/LeetCode_Arithmetic_Slices_II_SubSequences.java
+++ Problem Solving/LeetCode_Arithmetic_Slices_II_SubSequences.java
@@ -7,21 +7,22 @@ class LeetCode_Arithmetic_Slices_II_SubSequences {
     public static int numberOfArithmeticSlices(int[] nums) {
         int ans =0;
 
-        Map<Integer,Integer> maps[] = new Map[nums.length];
+        List<Map<Integer,Integer>> maps = new ArrayList<>();
         for(int i=0;i<nums.length;i++){
-            maps[i]=new HashMap<>();
+            Map<Integer,Integer> currMap=new HashMap<>();
+            maps.add(currMap);
             for(int j=0;j<i;j++){
                 long diff = (long)nums[i]-(long)nums[j];
 
                 if(diff <= Integer.MIN_VALUE || diff >= Integer.MAX_VALUE )continue;
-                int mapEndingOnI = maps[i].getOrDefault((int)diff,0);
-                int mapEndingOnJ = maps[j].getOrDefault((int)diff,0);
+                int mapEndingOnI = maps.get(i).getOrDefault((int)diff,0);
+                int mapEndingOnJ = maps.get(j).getOrDefault((int)diff,0);
 
                 ans+=mapEndingOnJ;
-                maps[i].put((int)diff,mapEndingOnI + mapEndingOnJ + 1);
+                currMap.put((int)diff,mapEndingOnI + mapEndingOnJ + 1);
             }
         }
-
         return ans;
     }
 }
+//Map[] maps = list.toArray(new HashMap[list.size()]);
